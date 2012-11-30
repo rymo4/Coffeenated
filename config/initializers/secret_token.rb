5f68291f513fb189239b73f1a3244027f031f595
Ryder Moody <rydthemoodster@gmail.com>
2012-11-30 17:05:55 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeenated::Application.config.secret_token = '560d9170a2eeda988d2e821a0ea903cabf4e0258f8b4a529965691f884f330c0113538d31c7fde8e95549b0fdcd6aed53e73b6a0eae0eb7ec6d564e669f5641c'
