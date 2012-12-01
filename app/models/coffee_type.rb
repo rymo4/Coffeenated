186f4c3f33b045b1ee8fac0a60cc534b6cf6f18a
Ryder Moody <rydthemoodster@gmail.com>
2012-11-30 21:12:55 -0500

    Moves maps method onto coffee instead of rankings

--- app/models/coffee_type.rb
+++ app/models/coffee_type.rb
@@ -5,4 +5,15 @@ class CoffeeType
   belongs_to :roaster
   has_many :rankings
 
+  def maps_url
+    'http://maps.googleapis.com/maps/api/staticmap?' + {
+      center: 'Brooklyn+Bridge,New+York,NY',
+      zoom: 13,
+      size: '400x400',
+      maptype: 'roadmap',
+      markers: 'color:blue%7label:S%7C40.702147,-74.015794',
+      sensor: 'false'
+    }.inject('') {|str, opt| str << '&' << opt.join('=') }
+  end
+
 end
